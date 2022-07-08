9117e64a4aed48758e92dfe977d8a333cba68ca1
Jack Mogen <jmogen@uwaterloo.ca>
2022-07-08 14:17:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Card_Counting
+ A little program to count cards for different card games
---
+++ automated_blackjack.py
@@ -0,0 +1,121 @@
+from sre_constants import JUMP
+from turtle import goto
+import requests
+
+
+def generate_deck(count):
+    response = requests.post(f"https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count={count}")
+    data = response.json()
+    deckid = data['deck_id']
+    return deckid
+
+def draw_card(deckid, cards):
+    response = requests.post(f"https://deckofcardsapi.com/api/deck/{deckid}/draw/?count={cards}")
+    data = response.json()
+    # if data['remaining'] <= 10:
+    #     reshuffle(deckid)
+    # print('SECOND', response.content, '\n')
+    # print(data['cards'][0]['code'])
+    return data
+
+def reshuffle(deckid):
+    requests.post(f"https://deckofcardsapi.com/api/deck/{deckid}/shuffle")
+    return None
+
+def convert(data):
+    if data[0] == 'A':
+        return 11
+    if data[0] == 'K':
+        return 10
+    if data[0] == 'Q':
+        return 10
+    if data[0] == 'J':
+        return 10
+    if data[0] == '0':
+        return 10
+    else:
+        return int(data[0])
+
+#d31xyjxoan40
+
+def manual_play():
+    while True:
+        print('Enter 1 to play BlackJack')
+        print("Enter any key to exit\n")
+        if input() == "1":
+            deck_id = generate_deck(1)
+
+            print(f"Game: {game}")
+            game += 1
+            print("Enter 1 to draw")
+            print("Enter any key to exit\n")
+            if input() == "1":
+                player_hand_count = 0
+                #first card
+                card = draw_card(deck_id, 1)
+                player_hand_count += convert(card['cards'][0]['code'])
+                #second card
+                card = draw_card(deck_id, 1)
+                player_hand_count += convert(card['cards'][0]['code'])
+
+                while True:
+                    print(f"Hand Strength: {player_hand_count}")
+                    print("Enter 1 to hit")
+                    print("Enter any key to stay\n")
+                    if input() == "1":
+                        card = draw_card(deck_id, 1)
+                        player_hand_count += convert(card['cards'][0]['code'])
+                        
+                    else:
+                        break 
+                if player_hand_count > 21:
+                    print("Dealer Wins !\n\n")
+                    dealer_hand_count = 0
+                    #first card
+                    card = draw_card(deck_id, 1)
+                    dealer_hand_count += convert(card['cards'][0]['code'])
+                    #second card
+                    card = draw_card(deck_id, 1)
+                    dealer_hand_count += convert(card['cards'][0]['code'])
+                else:
+                    dealer_hand_count = 0
+                    #first card
+                    card = draw_card(deck_id, 1)
+                    dealer_hand_count += convert(card['cards'][0]['code'])
+                    #second card
+                    card = draw_card(deck_id, 1)
+                    dealer_hand_count += convert(card['cards'][0]['code'])
+                    print("Dealer Strength:", dealer_hand_count)
+                    while dealer_hand_count < 16:
+                        card = draw_card(deck_id, 1)
+                        dealer_hand_count += convert(card['cards'][0]['code'])
+                        print("Dealer Strength:", dealer_hand_count)
+                    if ((dealer_hand_count > 21) or (player_hand_count > dealer_hand_count)):
+                        print("Player Wins !\n\n")
+                    else:
+                        print("Dealer Wins !\n\n")
+                
+
+            else:
+                break
+        else: 
+            break
+        
+def automated_play():
+    print("Sorry, not implemented yet!")
+    return None
+
+#event loop
+game = 1
+money_to_bet = 1000
+
+while True:
+    print("Enter 1 for manual play")
+    print("Enter 2 for card automated play")
+    print("Enter any key to exit\n")
+    if input() == "1":
+        manual_play()
+    elif input() == "2":
+        automated_play()
+    else:
+        break
